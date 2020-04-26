c51e33132d2ee01f08720fa70d54729de96eb8df
Matt Huston <matthuston1@aol.com>
2020-04-26 17:49:48 -0600

    trying to refactor code

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-const synth = new Tone.PolySynth().toMaster()
+const synth = new Tone.Synth();
+synth.oscillator.type = 'sine';
+synth.toMaster()
 
 const notes = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
 
 let html = ''
+let key = ''
+console.log(key)
+const container = document.getElementById('container')
 
 for (let octave = 0; octave < 2; octave++) {
 
@@ -13,9 +18,35 @@ for (let octave = 0; octave < 2; octave++) {
     if (note == 'E' || note == 'B') {
       hasSharp = false
     }
+    //testing
+    // let whites = document.createElement('div')
+    // console.log(whites)
+    // // whites.classList.add('whitenote')
+    // whites.setAttribute('class', 'whitenote')
+    // whites.addEventListener('mousedown', noteDown(this, false))
+    // whites.addEventListener('mouseup', noteUp(this, false))
+    // whites.addEventListener('mouseleave', noteUp(this, false))
+    // whites.setAttribute('data', `note: '${note + (octave + 4)}'`)
+    // container.appendChild(whites)
+
+
     html += `<div class='whitenote' onmousedown='noteDown(this, false)' onmouseup='noteUp(this, false)' onmouseleave='noteUp(this, false)' data-note='${note + (octave + 4)}'>`
+    let whiteNote = document.querySelector('.whitenote')
+
     if (hasSharp) {
-      html += `<div class='blacknote' onmousedown='noteDown(this, true)' onmouseup='noteUp(this, true)' onmouseleave='noteUp(this, true)' data-note='${note + '#' + (octave + 4)}'></div>`
+      //testing
+      // let blacks = document.createElement('div')
+      // console.log(blacks)
+      // // blacks.classList.add('blacknote')
+      // blacks.setAttribute('class', 'blacknote')
+      // blacks.addEventListener('mousedown', noteDown(this, true))
+      // blacks.addEventListener('mouseup', noteUp(this, true))
+      // blacks.addEventListener('mouseleave', noteUp(this, true))
+      // blacks.setAttribute('data', `note: '${note + '#' + (octave + 4)}'`)
+      // container.appendChild(blacks)
+
+      html += `<div class='blacknote' onmousedown='noteDown(this, true)' onmouseup='noteUp(this, true)' onmouseleave='noteUp(this, true)' data-note='${note + '#' + (octave + 4)}' data-='${key}'></div>`
+      let blackNote = document.querySelector('.blacknote')
     }
     html += `</div>`
 
@@ -30,6 +61,7 @@ function noteUp(elem, isSharp) {
 }
 
 function noteDown(elem, isSharp) {
+  console.log(elem.dataset)
   let note = elem.dataset.note
   elem.style.background = isSharp ? '#777' : '#ccc'
   // synth.triggerAttackRelease(note, '16n')
@@ -38,8 +70,15 @@ function noteDown(elem, isSharp) {
   event.stopPropagation()
 }
 
+// whiteNote.addEventListener('keydown', whiteKeyDown(this, true))
+
+// function whiteKeyDown(elem, isSharp) {
+//   e.style.background = isSharp ? '#777' : '#ccc'
+
+// }
+
 document.addEventListener("keydown", e => {
-  console.log(e)
+
   switch (e.key) {
     case "d":
       return synth.triggerAttack("C4");
@@ -71,19 +110,52 @@ document.addEventListener("keydown", e => {
 });
 
 document.addEventListener("keyup", e => {
-  // switch (e.key) {
-  //   case "d":
-  //   case "r":
-  //   case "f":
-  //   case "t":
-  //   case "g":
-  //   case "h":
-  //   case "u":
-  //   case "j":
-  //   case "i":
-  //   case "k":
-  //   case "o":
-  //   case "l":
+
+  switch (e.key) {
+    case "d":
+    case "r":
+    case "f":
+    case "t":
+    case "g":
+    case "h":
+    case "u":
+    case "j":
+    case "i":
+    case "k":
+    case "o":
+    case "l":
       synth.triggerRelease();
-  // }
+  }
 });
+
+function keyNote(note) {
+  switch (note) {
+    case "C4":
+      key = 'd';
+    case "C#4":
+      key = 'r';
+    case "D4":
+      key = 'f';
+    case "D#4":
+      key = 't';
+    case "E4":
+      key = 'g';
+    case "F4":
+      key = 'h';
+    case "F#4":
+      key = 'u';
+    case "G4":
+      key = 'j';
+    case "G#4":
+      key = 'i';
+    case "A4":
+      key = 'k';
+    case "A#4":
+      key = 'o';
+    case "B4":
+      key = 'l';
+    default:
+      return;
+  }
+}
+
